521029500c43f4d6b1e90c655da173872480d342
tuananh2005889 <tuannguyen.120910@gmail.com>
2024-11-01 16:31:47 +0700

    update add product and còn cái đóng model chưa fix

--- shoes_shop/client/src/App.js
+++ shoes_shop/client/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // Nhập QueryClient và QueryClientProvider
 import Home from './Homepage/HomepageLayout';
-import Admin from './admin/AdminLayout'
+import Admin from './admin/AdminLayout';
+
+// Tạo một instance của QueryClient
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </div>
+    <QueryClientProvider client={queryClient}> 
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- shoes_shop/client/src/admin/Product/ProductLayout.jsx
+++ shoes_shop/client/src/admin/Product/ProductLayout.jsx
@@ -1,66 +1,190 @@
 import React, { useState } from "react";
 import { Input } from "antd";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useRef } from "react";
+import toast, { Toaster } from "react-hot-toast";
+
 const ProductLayout = () => {
-	const [isopen, setIsOpen] = useState(false);
+	const [formData, setFormData] = useState({
+		Name: "",
+		Bio: "",
+		Brand: "",
+		Price: "",
+		Quantity: "",
+		Size: [],
+		Color: [],
+		Image: [],
+	});
 
-	const handleOpen = () => {
-		setIsOpen(true);
-	};
-	const handleClose = () => {
-		setIsOpen(false);
+	const [imgs, setImgs] = useState([]);
+	const [colors, setColors] = useState([]);
+
+	const imgRef = useRef(null);
+	const queryClient = useQueryClient();
+	/////////////////////////////////////////////////////////////////////////////////
+	const {
+		mutate: addShose,
+		isError,
+		error,
+		isPending,
+	} = useMutation({
+		mutation: async (formData) => {
+			try {
+				const res = await fetch("/api/shoes/add", {
+					method: "POST",
+					headers: {
+						"content-Type": "application/json",
+					},
+					body: JSON.stringify(formData),
+				});
+
+				const data = await res.json();
+				if (!res.ok)
+					throw new Error(data.error || "Failed to create Shoes");
+				console.log(data);
+				return data;
+			} catch (error) {
+				throw new Error(error.message);
+			}
+		},
+		onSuccess: () => {
+			setFormData({
+				Name: "",
+				Bio: "",
+				Brand: "",
+				Price: "",
+				Quantity: "",
+				Size: [],
+				Color: [],
+				Image: [],
+			});
+			closeModal();
+
+
+			queryClient.invalidateQueries(["Product"]);
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		},
+	});
+
+	///////////////////////////////////////////////
+	//  getALLProduct
+	const {
+		data: product,
+		isloading,
+		refetch,
+		isRefetching,
+	} = useQuery({
+		queryKey: ["product"],
+		queryFn: async () => {
+			try {
+				const response = await fetch("/api/shoes/all");
+				const data = await response.json();
+
+				console.log(data);
+
+				if (!response.ok) {
+					throw new Error(data.message || "Something went wrong!");
+				}
+
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+	});
+	//////////////////////////////////////////////////
+	const handleInputChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
 
+	const handleSubmit = () => {
+		// Send formData to the server
+		formData.Image = imgs;
+		formData.Color = colors;
+		console.log(formData);
+		addShose(formData);
+	};
+	////////////////////////////////////////////////
+	const handleImgChange = (e) => {
+		const files = Array.from(e.target.files);
+		const updatedImgs = [...imgs];
+
+		files.forEach((file) => {
+			const reader = new FileReader();
+			reader.onload = () => {
+				updatedImgs.push(reader.result);
+				// setImgs(updatedImgs);
+				setImgs((prevImgs) => [...prevImgs, reader.result]);
+			};
+			reader.readAsDataURL(file);
+		});
+	};
+
+	const handleRemoveImg = (indexToRemove) => {
+		setImgs(imgs.filter((_, index) => index !== indexToRemove));
+	};
+	//////////////////////////////////////
+
+	// open div add
+	const closeModal = () => {
+		const modal = document.getElementById("Add_Car");
+		if (modal) {
+			modal.close();
+		}
+	};
 	return (
 		<div>
-			<div className="p-12 w-full justify-end flex" a>
+			<div className="p-12 w-full justify-end flex">
 				<div
 					className="bg-neutral-600 h-12 rounded-xl font-bold cursor-pointer hover:scale-105 transition-all ease-in-out duration-200 w-[120px] text-white p-3 hover:bg-white hover:text-black"
-					onClick={handleOpen}
+					onClick={() =>
+						document.getElementById("Add_shoes").showModal()
+					}
 				>
 					Add product
 				</div>
 			</div>
-			{isopen && (
-				<div className="flex z-50 absolute justify-center items-center w-full">
-					<div className="bg-gradient-to-r from-neutral-800 to-orange-800 rounded-xl w-[800px] p-5">
-						<div className="flex justify-end p-2  text-white text-2xl">
-							<p
-								onClick={handleClose}
-								className="cursor-pointer hover:text-red-500 transition-all duration-200 ease-in-out"
-							>
-								X
-							</p>
-						</div>
-						<div className="text-white grid-cols-[15%_85%] grid text-xl gap-y-6 overflow-auto px-12">
-							<p className="">ID:</p>
-							<textarea className="bg-gradient-to-l from-neutral-200 to-orange-300 textarea textarea-bordered h-[25px] shadow-md shadow-white text-black w-full" />
+			<dialog id="Add_shoes" className="modal ">
+			<div className="modal-box backdrop-blur-3xl bg-gray-700 shadow-gray-500 shadow-md bg-opacity-0  w-full ">
+					<div className="bg-neutral-800 w-[800px] rounded-xl p-12">
+						<div className="text-white grid-cols-[30%_70%] grid text-xl gap-y-6 px-2 overflow-auto">
+							<p className="mt-1">ID:</p>
+							<input className="bg-neutral-500 mt-1 textarea textarea-bordered  rounded-md text-black w-full"/>
 
 							<p>Name:</p>
-							<textarea className="bg-gradient-to-l from-neutral-200 to-orange-300 textarea textarea-bordered h-[25px] shadow-md shadow-white text-black w-full" />
+							<input className="bg-neutral-500 textarea textarea-bordered  rounded-md text-black w-full"/>
 
 							<p>Bio:</p>
-							<textarea className="bg-gradient-to-l from-neutral-200 to-orange-300 textarea textarea-bordered h-[25px] shadow-md shadow-white text-black w-full" />
+							<input className="bg-neutral-500 textarea textarea-bordered  rounded-md text-black w-full"/>
 							<p>Brand:</p>
-							<textarea className="bg-gradient-to-l from-neutral-200 to-orange-300 textarea textarea-bordered h-[25px] shadow-md shadow-white text-black w-full" />
+							<input className="bg-neutral-500 textarea textarea-bordered  rounded-md text-black w-full"/>
 
 							<p>Price:</p>
-							<textarea className="bg-gradient-to-l from-neutral-200 to-orange-300 textarea textarea-bordered h-[25px] shadow-md shadow-white text-black w-full" />
+							<input className="bg-neutral-500 textarea textarea-bordered  rounded-md text-black w-full"/>
 							<p>Quantity:</p>
-							<textarea className="bg-gradient-to-l from-neutral-200 to-orange-300 textarea textarea-bordered h-[25px] shadow-md shadow-white text-black w-full" />
+							<input className="bg-neutral-500 textarea textarea-bordered  rounded-md text-black w-full"/>
 
 							<p>Size:</p>
-							<textarea className="bg-gradient-to-l from-neutral-200 to-orange-300 textarea textarea-bordered h-[25px] shadow-md shadow-white text-black w-full" />
+							<input className="bg-neutral-500 textarea textarea-bordered  rounded-md text-black w-full"/>
 
 							<p>Color:</p>
-							<textarea className="bg-gradient-to-l from-neutral-200 to-orange-300 textarea textarea-bordered h-[25px] shadow-md shadow-white text-black w-full" />
-
+							<input className="bg-neutral-500 textarea textarea-bordered  rounded-md text-black w-full"/>
 							<p>Image:</p>
-							<textarea className="bg-gradient-to-l from-neutral-200 to-orange-300 textarea textarea-bordered h-[25px] shadow-md shadow-white text-black w-full" />
+							<input className="bg-neutral-500 textarea textarea-bordered  rounded-md text-black w-full"/>
 						</div>
 					</div>
 				</div>
-			)}
-			;
+				<div
+					method="dialog"
+					className="modal-backdrop z-auto bg-white absolute h-screen w-screen"
+				>
+					<button className="">Close</button>
+				</div>
+			</dialog>
+			
+
 		</div>
 	);
 };
